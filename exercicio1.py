1643651650b87e421d751197af83f02fdd6e0589
Lucas Lima <57077913+astatonn@users.noreply.github.com>
2020-10-12 13:49:07 -0300

    Lista de exercícios para que sejam corrigidos.

---
+++ exercicio1.py
@@ -0,0 +1,14 @@
+# FAÇA UM PROGRAMA QUE MOSTRE A QUANTIDADE DE NÚMEROS DA SEQUENCIA DE FIBONNACI
+
+def fibonnaci (numero):
+    a, b = 0, 1
+    while a < numero:
+        print(a, end=',')
+        a, b = b, a+b
+
+
+# while True:
+#     n=print(" Digite Quantos termos você deseja :")
+
+# print ('Fim do programa')
+fibonnaci(10)
---
+++ exercicio4.py
@@ -0,0 +1,12 @@
+# FAÇA UM PROGRAMA QUE UMA QUANTIDADE DINÂMICA DE NOTAS E MOSTRA A MÉDIA DOS VALORES
+nota=0
+media=0
+while (True){
+    nota=float(input('Digite a nota: '))
+    opcao=input('Deseja continuar? (yes|no)').capitalize()
+    if opcao = 'no':
+        break
+    }
+for i in range 0,len(nota):
+    media=media+nota[media]
+print ('A média é: {}'.format(media/len(nota)))
