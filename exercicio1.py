4123b33ca6c33b9ac3aae1d1c782ea806e616fe1
Lucas Lima <57077913+astatonn@users.noreply.github.com>
2020-10-27 14:24:50 -0300

    Exercícios para correção

---
+++ exercicio1.py
@@ -0,0 +1,4 @@
+# FAÇA UM PROGRAMA QUE LEIA O NOME E A IDADE E MOSTRE NA TELA
+
+    print('Seu nome é {}'.format(input('Digite seu nome:')))
+    print('Sua idade é {}'.format(input('Digite sua idade:')))
